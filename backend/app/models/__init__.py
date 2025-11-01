e7a4439fd95b9b6655fc6cddc40bba22ea9b64bc
janus qi <baksmalin@gmail.com>
2025-11-01 11:56:13 +0200

    refactor: export current response models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 """Pydantic models for request/response validation."""
 
-from .schemas import StructuredDoc, StructureRequest, StructureResponse
+from .schemas import MultiModelResponse, StructuredDoc, StructureRequest
 
-__all__ = ["StructuredDoc", "StructureRequest", "StructureResponse"]
+__all__ = ["StructuredDoc", "StructureRequest", "MultiModelResponse"]
 
